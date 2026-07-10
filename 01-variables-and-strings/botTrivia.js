9edb2ca1904f2783239ccf0b57e1e81d8d40b932
ZSvll <paperlyx.org@gmail.com>
2026-07-09 22:56:11 -0400

    Variables and Strings

---
+++ 01-variables-and-strings/botTrivia.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Zsvll";
+const botLocation = "Rep. Dom.";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = ("Sabias que " + favoriteLanguage + " es un lenguaje de programacion orientado a objetos.");
+
+console.log(codingFact);
+
+codingFact = ("Nunca olvides poner el punto y coma en " + favoriteLanguage + ".");
+
+console.log(codingFact);
+
+codingFact = ("Se recomienda usar camelCase para nombrar las variables en " + favoriteLanguage + ".");
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ 01-variables-and-strings/sentenceCreator.js
@@ -0,0 +1,22 @@
+let 
+adjective = "beautiful ",
+noun = "beach ",
+verb = "Y",
+place = "Z",
+adjective2 = "X",
+noun2 = "sea";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory);
+
+adjective = "a";
+noun = "b";
+verb = "c";
+place = "d";
+adjective2 = "f";
+noun2 = "g";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " +  secondStory);
